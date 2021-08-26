b3313a6c54c9a7c44a8572faa168a921720237a8
huuhung99 <bacxuan2005@gmail.com>
2021-08-26 11:40:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Con gà Nhật!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/huuhung99"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click vô!
         </a>
       </header>
     </div>
